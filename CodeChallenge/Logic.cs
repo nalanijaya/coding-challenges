40c8a4d2b5ec9d98aa75a45be66f195e5a024c35
NJ <nalani.jayasundara@gmail.com>
2019-09-22 17:32:38 +0530

    How to find pair with particular sum

---
+++ CodeChallenge/Logic.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodeChallenge
+{
+    public class Logic
+    {
+        public static bool HasPairWithSum(List<int> numList, int sum)
+        {
+            HashSet<int> complements = new HashSet<int>();
+            foreach(var item in numList)
+            {
+                if(complements.Contains(item))
+                {
+                    Console.WriteLine("Pair Available");
+                    return true;
+                }
+                complements.Add(sum - item);
+            }
+            Console.WriteLine("Pair Not Available");
+            return false;
+        }
+    }
+}
---
+++ CodeChallengeConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using CodeChallenge;
+using System;
+using System.Collections.Generic;
+
+namespace CodeChallengeConsoleApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numList1 = new List<int>() { 1, 2, 4, 4 };
+            List<int> numList2 = new List<int>() { 1, 2, 2, 6 };
+            Logic.HasPairWithSum(numList2, 8);
+            Console.ReadLine();
+        }
+    }
+}
