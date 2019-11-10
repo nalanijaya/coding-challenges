39ec47295528351541c27c44c4585c68a6abdf2c
NJ <nalani.jayasundara@gmail.com>
2019-11-10 21:26:42 +0530

    Hour glass problem

--- CodeChallenge/Logic.cs
+++ CodeChallenge/Logic.cs
@@ -320,5 +320,36 @@ namespace CodeChallenge
             return orderTime.OrderBy(x => x.Value).
                 Select(y => y.Key).ToArray();
         }
+
+        public static int HourglassSum(int[][] mat)
+        {
+            int R = 5;
+            int C = 5;
+            if (R < 3 || C < 3)
+                return -1;
+
+            // Here loop runs (R-2)*(C-2)  
+            // times considering different 
+            // top left cells of hour glasses. 
+            int max_sum = int.MinValue;
+            for (int i = 0; i < R - 2; i++)
+            {
+                for (int j = 0; j < C - 2; j++)
+                {
+                    // Considering mat[i][j] as top  
+                    // left cell of hour glass. 
+                    int sum = (mat[i][j] + mat[i][j + 1] +
+                               mat[i][ j + 2]) + (mat[i + 1][ j + 1]) +
+                              (mat[i + 2][j] + mat[i + 2][ j + 1] +
+                               mat[i + 2][ j + 2]);
+
+                    // If previous sum is less then  
+                    // current sum then update 
+                    // new sum in max_sum 
+                    max_sum = Math.Max(max_sum, sum);
+                }
+            }
+            return max_sum;
+        }
     }
 }
